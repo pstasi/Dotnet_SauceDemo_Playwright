2841db5ce839bf5ef4b8127b3cee31bcac729ad4
Peter <peterjstasi@gmail.com>
2025-12-02 14:46:01 -0500

    Add project files.

---
+++ pages/HomePage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Playwright;
+
+namespace firstPlaywrightTest.pages
+{
+    public class HomePage
+    {
+        private readonly IPage _page;
+        private readonly ILocator _usernameInput;
+        private readonly ILocator _passwordInput;
+        private readonly ILocator _loginButton;
+        public readonly ILocator _errorMessage;
+
+
+        public HomePage(IPage page)
+        {
+            _page = page;
+            _usernameInput = page.GetByRole(AriaRole.Textbox, new() { Name = "Username"});
+            _passwordInput = page.GetByRole(AriaRole.Textbox, new() { Name = "Password"});
+            _loginButton = page.Locator("//input[@id='login-button']");
+            _errorMessage = page.Locator("//*[@data-test='error']");
+
+        }
+
+        public async Task EnterUsernameIntoField()
+        {
+            await _usernameInput.FillAsync("standard_user");
+        }
+
+        public async Task EnterPasswordIntoField()
+        {
+            await _passwordInput.FillAsync("secret_sauce");
+        }
+
+        public async Task ClickLoginButton()
+        {
+            await _loginButton.ClickAsync();
+        }
+
+        public async Task AttemptLogin()
+        {
+            await EnterUsernameIntoField();
+            await EnterPasswordIntoField();
+            await ClickLoginButton();
+        }
+
+        public async Task<string> GetHomePageErrorMessage()
+        {
+            return await _errorMessage.InnerTextAsync();
+        }
+    }
+}
---
+++ tests/HomePageTests.cs
@@ -0,0 +1,55 @@
+﻿using firstPlaywrightTest.pages;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace firstPlaywrightTest.tests
+{
+    [Category("HomePageTests")]
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class ExampleTest : PageTest
+    {
+        private IPage _page;
+        private HomePage _homePage;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _page = Page;
+            _homePage = new HomePage(_page);
+            await _page.GotoAsync("https://saucedemo.com");
+        }
+
+        [TearDown]
+        public async Task Teardown()
+        {
+            await _page.CloseAsync();
+        }
+
+        [Test]
+        [Category("Regression")]
+        public async Task ValidateErrorMessageNoUserNoPassword()
+        {
+            Console.WriteLine("ValidateErrorMessageNoUserPassword");
+            await _homePage.ClickLoginButton();
+            string? errorMessage = await _homePage.GetHomePageErrorMessage();
+            Console.WriteLine(errorMessage);
+            if (errorMessage != "Epic sadface: Username is required")
+            {
+                throw new Exception("Error!");
+            }
+        }
+
+        [Test]
+        [Category("Login")]
+        public async Task AttemptLoginTest()
+        {
+            Console.WriteLine("running attemptlogintest");
+            await _homePage.AttemptLogin();
+        }
+    }
+}
